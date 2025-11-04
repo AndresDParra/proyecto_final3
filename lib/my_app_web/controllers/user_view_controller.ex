9e56d9d3515ce362fec319c6a5c3a65d0cff106f
Andres David Parra Parra <andresdavidparraparra@Andress-MacBook-Air.local>
2025-11-03 19:50:10 -0500

    estable

--- lib/my_app_web/controllers/user_view_controller.ex
+++ lib/my_app_web/controllers/user_view_controller.ex
@@ -2,28 +2,32 @@ defmodule MyAppWeb.UserViewController do
   use MyAppWeb, :controller
 
   def index(conn, _params) do
-    MyApp.Auth.list_users()
-    render(conn, :index)
+    users = MyApp.Auth.list_users()
+
+    conn
+    |> json(%{
+      users:
+        Enum.map(users, fn user ->
+          %{
+            id: user.id,
+            username: user.username,
+            name: user.name
+            # Don't include password in JSON response!
+          }
+        end)
+    })
   end
 
   def show(conn, %{"id" => id}) do
-    # id = String.to_integer(id)
-    case Mytest.get_user(id) do
-      nil ->
-        conn
-        |> json(%{error: "User not found"})
-        |> redirect(to: ~p"/Users/")
+    # Will show 404 page if not found
+    user = MyApp.Auth.get_user!(id)
 
-      user ->
-        conn
-        |> json(%{
-          user: %{
-            id: user.id,
-            username: user.username,
-            name: user.name,
-            password: user.password
-          }
-        })
-    end
+    json(conn, %{
+      user: %{
+        id: user.id,
+        username: user.username,
+        name: user.name
+      }
+    })
   end
 end
