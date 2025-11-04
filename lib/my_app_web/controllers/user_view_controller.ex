4f372da01ca6bed811a6eb01c61f6c426c32900e
Andres David Parra Parra <andresdavidparraparra@Andress-MacBook-Air.local>
2025-11-04 15:48:48 -0500

    listo para integración del login

--- lib/my_app_web/controllers/user_view_controller.ex
+++ lib/my_app_web/controllers/user_view_controller.ex
@@ -18,16 +18,56 @@ defmodule MyAppWeb.UserViewController do
     })
   end
 
-  def show(conn, %{"id" => id}) do
-    # Will show 404 page if not found
-    user = MyApp.Auth.get_user!(id)
+  def show(conn, %{"id" => id_param}) do
+    case Integer.parse(id_param) do
+      {id, ""} ->
+        case safe_get_user(id) do
+          {:ok, user} ->
+            conn
+            |> json(%{
+              data: [
+                %{
+                  type: "user",
+                  id: to_string(user.id),
+                  attributes: %{
+                    role_id: user.role_id,
+                    name: user.name,
+                    username: user.username
+                  }
+                }
+              ]
+            })
 
-    json(conn, %{
-      user: %{
-        id: user.id,
-        username: user.username,
-        name: user.name
-      }
-    })
+          {:error, :not_found} ->
+            conn
+            |> put_status(:not_found)
+            |> json(%{error: "User not found"})
+
+          {:error, reason} ->
+            conn
+            |> put_status(:internal_server_error)
+            |> json(%{error: "Error retrieving user: #{reason}"})
+        end
+
+      _ ->
+        conn
+        |> put_status(:bad_request)
+        |> json(%{error: "Invalid ID format"})
+    end
+  end
+
+  defp safe_get_user(id) do
+    try do
+      case Mytest.get_user(id) do
+        nil -> {:error, :not_found}
+        user -> {:ok, user}
+      end
+    rescue
+      e in Ecto.Query.CastError ->
+        {:error, "Invalid ID type: #{Exception.message(e)}"}
+
+      e ->
+        {:error, Exception.message(e)}
+    end
   end
 end
